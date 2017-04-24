5cf0528f8cb80375983528ccb88a289a68974a54
Rodrigo Cisternas <rodcisal@gmail.com>
2017-04-23 22:27:17 -0300

    first

---
+++ src/ChevronRight.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AngleRight from 'react-icons/lib/fa/angle-right'
+
+export default class ChevronRight extends React.Component {
+
+  static propTypes = {
+
+  }
+
+  render () {
+    return (
+      <AngleRight />
+    )
+  }
+
+}
