52173a8ec395996d5be351b68e5a3ffcd9319c2b
Rodrigo Cisternas <rodcisal@gmail.com>
2017-05-09 01:05:22 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -39,6 +39,26 @@ export default class App extends Component {
     })
   }
 
+  state = {
+    currentSection: null
+  }
+
+  renderNavBar () {
+    return (
+      <div className='siteNavBar'>
+        <h1 style={{cursor: 'pointer'}} onClick={() => this.setState({currentSection: null})}> Paulina Sayago </h1>
+        <div>
+          <div
+            style={{cursor: 'pointer'}}
+            onClick={() => this.setState({currentSection: 'sobreMi'})}>SOBRE MI</div>
+          <div
+            style={{cursor: 'pointer'}}
+            onClick={() => this.setState({currentSection: 'sesiones'})}>SESIONES</div>
+        </div>
+      </div>
+    )
+  }
+
   render () {
     const mainTextStyles = {
       fontSize: 48,
@@ -54,15 +74,125 @@ export default class App extends Component {
       nextArrow: <SamplePrevArrow {...this.props} />
     }
 
-    return (
-      <div className='App'>
-        <div className='siteNavBar'>
-          <h1> Paulina Sayago </h1>
-          <div>
-            <div>SOBRE MI</div>
-            <div>SESIONES</div>
+    if (this.state.currentSection === 'sesiones') {
+      return (
+        <div className='App'>
+          {this.renderNavBar()}
+
+          <div className='sesiones'>
+            <div
+              style={{
+                backgroundImage: 'url(/photos/sesiones/chinito/9.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: '-184px center'
+              }}
+              className='block'
+              onClick={() => this.setState({currentSection: 'chinito'})}>
+              </div>
+            <div
+              style={{
+                backgroundImage: 'url(/photos/sesiones/dieguito/1.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: '-27px center'
+              }}
+              className='block'
+              onClick={() => this.setState({currentSection: 'dieguito'})}>
+              </div>
+            <div
+              style={{
+                backgroundImage: 'url(/photos/sesiones/mia/mia4.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: '-10px center'
+              }}
+              className='block'
+              onClick={() => this.setState({currentSection: 'mia'})}>
+              </div>
           </div>
         </div>
+      )
+    }
+
+    if (this.state.currentSection === 'chinito') {
+      const photos = 14
+      let results = []
+      for (let i = 1; i < photos + 1; i++) {
+        results.push(
+          <div>
+            <img src={`/photos/sesiones/chinito/${i}.jpg`} />
+          </div>
+        )
+      }
+      return (
+        <div className='App'>
+          {this.renderNavBar()}
+          <h1 className='cumpleanos'>SESIÓN CHINITO</h1>
+          {results}
+        </div>
+      )
+    }
+
+    if (this.state.currentSection === 'dieguito') {
+      const photos = 19
+      let results = []
+      for (let i = 1; i < photos + 1; i++) {
+        results.push(
+          <div>
+            <img src={`/photos/sesiones/dieguito/${i}.jpg`} />
+          </div>
+        )
+      }
+      return (
+        <div className='App'>
+          {this.renderNavBar()}
+          <h1 className='cumpleanos'>CUMPLEAÑOS DIEGO</h1>
+          {results}
+        </div>
+      )
+    }
+
+    if (this.state.currentSection === 'mia') {
+      const photos = 18
+      let results = []
+      for (let i = 1; i < photos + 1; i++) {
+        results.push(
+          <div>
+            <img src={`/photos/sesiones/mia/mia${i}.jpg`} />
+          </div>
+        )
+      }
+      return (
+        <div className='App'>
+          {this.renderNavBar()}
+          <h1 className='cumpleanos'>CUMPLEAÑOS MIA</h1>
+          {results}
+        </div>
+      )
+    }
+
+    if (this.state.currentSection === 'sobreMi') {
+      return (
+        <div className='App'>
+          {this.renderNavBar()}
+          <div style={{marginTop: 30, fontSize: 22, padding: '0px 470px', color: 'grey', marginBottom: 100, fontWeight: 'lighter', textAlign: 'left'}}>
+            <img src='/sobre_mi.jpg' alt='' style={{maxWidth: '100%', marginBottom: 30}} />
+            <p>Hola soy Paulina nací en La Ligua, viví en La Serena y hoy disfruto Santiago.</p>
+            <p>Soy fotógrafa aficionada, mi mente comenzó a tomar fotos hasta que se hizo urgente llevarlo a la acción
+            y en poco tiempo se convirtío en pasión.</p>
+            <p>
+              Mientras menos posadas las fotos más me gustan y mejor quedan a mi parecer.
+            </p>
+            <p>Si te gusta lo que ves, conversemos:</p>
+            <div style={{marginTop: 30, color: '#9E9E9E'}}>
+              <a style={{color: '#9E9E9E'}} href='mailto:sayago1983@gmail.com'> sayago1983@gmail.com </a>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <div className='App'>
+        {this.renderNavBar()}
         <div className='container'>
           <Slider {...settings} ref='slider'>
             {photos.map((photo) => {
@@ -73,7 +203,7 @@ export default class App extends Component {
               )
             })}
           </Slider>
-          <div style={{marginTop: 30, color: '#9E9E9E' }}>
+          <div style={{marginTop: 30, color: '#9E9E9E'}}>
             <a style={{color: '#9E9E9E'}} href='mailto:sayago1983@gmail.com'> sayago1983@gmail.com </a>
           </div>
         </div>
--- src/photos.js
+++ src/photos.js
@@ -1,20 +1,15 @@
 const photos = [
-  'mia1.jpg',
-  'mia2.jpg',
-  'mia3.jpg',
-  'mia4.jpg',
-  'mia5.jpg',
-  'mia6.jpg',
-  'mia7.jpg',
-  'mia8.jpg',
-  'mia9.jpg',
-  'mia10.jpg',
-  'mia11.jpg',
-  'mia18.jpg',
-  'mia12.jpg',
-  'mia13.jpg',
-  'mia14.jpg',
-  'mia15.jpg',
-  'mia17.jpg'
+  '001.jpg',
+  '002.jpg',
+  '004.jpg',
+  '005.jpg',
+  '006.jpg',
+  '007.jpg',
+  '008.jpg',
+  '009.jpg',
+  '010.jpg',
+  '011.jpg',
+  '012.jpg',
+  '013.jpg'
 ]
 export default photos
